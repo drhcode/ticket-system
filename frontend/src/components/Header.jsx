a0a459b70ff2b869fb8520e75ce6902a206fc84c
drhcode <doriancocka@gmail.com>
2023-02-21 17:31:14 +0100

    Started frontend

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,8 @@
+import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return <div>Header</div>;
+};
+
+export default Header;
